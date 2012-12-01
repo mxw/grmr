e82e1e8faef80a68e9ea0bbcf36de66a6a92c2b9
Max Wang <mxawng@gmail.com>
2012-12-01 05:22:21 -0500

    Make fidelity.rb better

--- src/fidelity.rb
+++ src/fidelity.rb
@@ -1,73 +1,69 @@
-# Complete analysis framework for comparing the fidelity between the compressed final string and the original
+#!/usr/local/bin/ruby
+
+#
+# fidelity.rb - Analysis framework for comparing the fidelity between a string
+# and its compressed output.
+#
 
 require 'rubygems'
 require 'bzip2'
 require 'descriptive_statistics'
 require 'levenshtein'
 
-#returns how large the string is after bzip compression
+USAGE = "Usage: ./fidelity.rb file1 file2"
+
+module Enumerable
+  def stats
+    { number: number,
+      min: min,
+      max: max,
+      median: median,
+      mean: mean,
+      stddev: standard_deviation,
+    }
+  end
+
+  def stats_s
+    stats.inject('') do |s, (k, v)|
+      s + "%7s " % (k.to_s + ':') + v.to_s + "\n"
+    end.chop
+  end
+end
+
 def entropy(s)
-  writer = Bzip2::Writer.new File.new('temp',mode="w")
-  writer << s
-  writer.close
-  return File.size('temp')
+  Bzip2.compress(s).size
 end
 
-def avgwordlen (s)
-  words = s.split /\s+/
-  words.map! {|w| w.length}
-  words.extend(DescriptiveStatistics)
-  return words.mean
+def avgwordlen(s)
+  s.split.map { |w| w.size }.mean
 end
 
-#statistics on the ngram distribution
-def ngrams (s, n)
-  ## maps ngrams to their counts
-  nmap = {}
-  len = s.length
-  curNGram = nil
-  for i in 0..(len-n)
-    curNGram = s[i,n]
-    if nmap.has_key? curNGram
-      nmap[curNGram] += 1
-    else
-      nmap[curNGram] = 1
-    end
-  end
-  counts = []
-  nmap.each do |key,val|
-    counts << val
-  end
-  counts.extend(DescriptiveStatistics)
-  return {
-    "num" => counts.number,
-    "mean" => counts.mean,
-    "max" => counts.max,
-    "stddev" => counts.standard_deviation
-  }
+def ngrams(s, n)
+  nmap = Hash.new(0)
+  s.chars.each_cons(n) { |ngram| nmap[ngram.join] += 1 }
+  nmap.values.stats_s
 end
 
-# assume we have ?_in and ?_out as the two files to compare
-def processCase (fprefix)
-  f1 = File.new(fprefix+"_in")
-  f2 = File.new(fprefix+"_out")
-  f3 = File.new(fprefix+"_res",mode="w")
-  str1 = f1.read
-  str2 = f2.read
-  len1 = str1.length
-  len2 = str2.length
-  maxlen = [len1,len2].max
-  f3.puts("Total Length: %d, %d" % [len1, len2])
-  f3.puts("Avg Word Lengths: %f, %f" % [avgwordlen(str1),avgwordlen(str2)])
-  for i in 1..3
-    f3.puts("%d-grams: %s, %s" % [i, ngrams(str1,i).to_s,ngrams(str2,i).to_s])
-  end
-  distfrac = Float(Levenshtein.distance(str1,str2)) / Float(maxlen)
-  f3.puts("Levenshtein: %f" % [distfrac])
-  f3.puts("Entropy: %d, %d" % [entropy(str1), entropy(str2)])
-  f1.close
-  f2.close
-  f3.close
+def analyze(fn0, fn1, fnout)
+  File.open(fn0) { |f0| File.open(fn1) { |f1| File.open(fnout, 'w+') { |fout|
+    ss = [f0.read, f1.read]
+    sizes = ss.map { |s| s.size }
+
+    [ "Total Length:      %d, %d" % sizes,
+      "Avg Word Length:   %f, %f" % ss.map { |s| avgwordlen s },
+      "Entropy:           %d, %d" % ss.map { |s| entropy s },
+      "Levenshtein:       %f" % [Levenshtein.distance(*ss) / sizes.max.to_f],
+      *(1..3).map do |i|
+        "%d-grams:\n%s\n%s" % [i, ngrams(ss[0], i), ngrams(ss[1], i)]
+      end
+    ].each { |s| fout.puts s }
+  } } }
 end
 
-processCase "test"
+if ARGV[0].nil? or ARGV[1].nil?
+  puts USAGE
+  exit
+end
+outfile = ARGV[2] || 'fid.out'
+
+analyze ARGV[0], ARGV[1], outfile
