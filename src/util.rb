534b6f72a1832c7f9ef7cc97e947d6cf32302146
Edward Gan <Efuzzy@gmail.com>
2012-12-02 19:02:16 -0500

    Added actual longest common substring for two raw string inputs

--- src/util.rb
+++ src/util.rb
@@ -3,39 +3,26 @@
 #
 
 #
-# Longest common substring implementation taken from
-# http://rosettacode.org/wiki/Longest_Common_Subsequence#Ruby
+# Longest common substring implementation adapted from
+# http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Longest_common_substring
 #
-def lcs(a, b)
-  lengths = Array.new(a.size + 1) { Array.new(b.size + 1) { 0 } }
 
-  # Row 0 and column 0 are initialized to 0 already.
-  a.split('').each_with_index do |x, i|
-    b.split('').each_with_index do |y, j|
-      if x == y
-        lengths[i+1][j+1] = lengths[i][j] + 1
+# Raw string lcs
+def lcs(s1, s2)
+  m = Array.new(s1.length+1){ [0] * (s2.length+1) }
+  longest, x_longest = 0,0
+  (1 .. s1.length).each do |x|
+    (1 .. s2.length).each do |y|
+      if s1[x-1] == s2[y-1]
+        m[x][y] = m[x-1][y-1] + 1
+        if m[x][y] > longest
+          longest = m[x][y]
+          x_longest = x
+        end
       else
-        lengths[i+1][j+1] = [lengths[i+1][j], lengths[i][j+1]].max
+        m[x][y] = 0
       end
     end
   end
-
-  # Read the substring out from the matrix.
-  result = ""
-  x, y = a.size, b.size
-
-  while x != 0 and y != 0
-    if lengths[x][y] == lengths[x-1][y]
-      x -= 1
-    elsif lengths[x][y] == lengths[x][y-1]
-      y -= 1
-    else
-      # Assert a[x-1] == b[y-1]
-      result << a[x-1]
-      x -= 1
-      y -= 1
-    end
-  end
-
-  result.reverse
+  s1[x_longest - longest .. (x_longest-1)]
 end
